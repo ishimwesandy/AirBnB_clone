3a477330ce6fb925e95f26babb3b28564c034e35
Sandrine Ishimwe <sandrineishimwe12@gmail.com>
2023-10-14 04:17:44 -0700

    adding State class

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""User Class Decalaration """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class Attributes
+        name (str): The name of the state.
+    """
+
+    name = ""

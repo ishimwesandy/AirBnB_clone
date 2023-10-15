d9470d49ab091ff0199f4bb49375e3759dbeba50
Sandrine Ishimwe <sandrineishimwe12@gmail.com>
2023-10-15 02:04:28 -0700

    adding City Class

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""City Class Decalaration"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class Attributes
+        state_id (str): 
+        name (str): The name of the city.
+    """
+
+    state_id = ""
+    name = ""

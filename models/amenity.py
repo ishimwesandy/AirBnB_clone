2a06d0777f3511bee9b9d2df9d0335c47c5c48a3
Sandrine Ishimwe <sandrineishimwe12@gmail.com>
2023-10-15 02:02:03 -0700

    adding Amenity Class

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Amenity Class Decalaration"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Attributes
+        name (str): name of the amenity
+    """
+
+    name = ""
